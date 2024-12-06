e4eaae0d26b9bed2979c0be41e004e943e155c1f
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-06 09:33:37 -0300

    feat: add components to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import { ContactMe } from "@/components/contact-me";
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/header";
+import { Links } from "@/components/links";
+
 export default function Home() {
-  return <section></section>;
+  return (
+    <section className="w-full min-h-screen bg-[#f6f6f6]">
+      <main className="max-w-xl mx-auto pt-24 pb-6 px-2 text-center flex flex-col gap-8">
+        <Header />
+        <Links />
+        <ContactMe />
+        <Footer />
+      </main>
+    </section>
+  );
 }
