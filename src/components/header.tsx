7663bda2c9dfa6e546239c0f76471cd01500a3e3
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-06 09:33:01 -0300

    feat: add avatar to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,26 @@
+import Image from "next/image";
 import { Icons } from "./icons";
 
 export function Header() {
   return (
-    <div className="flex flex-col gap-4">
-      <div className="rounded">...</div>
+    <header className="flex flex-col items-center gap-4">
+      <div className="p-1 border-2 rounded-full border-purple-800">
+        <Image
+          alt="avatar"
+          src={"/avatar.jpg"}
+          width={150}
+          height={150}
+          className="rounded-full"
+        />
+      </div>
       <h2 className="text-5xl md:text-7xl font-extrabold">Jessica Bandeira</h2>
       <p className="text-purple-800 text-xl md:text-3xl font-bold">
         Desenvolvedora Web
       </p>
-      <p className="text-gray-500">💻 Coding Adventurer | 🚀 Lifelong Learner</p>
+      <p className="text-gray-500">
+        💻 Coding Adventurer | 🚀 Lifelong Learner
+      </p>
       <Icons />
-    </div>
+    </header>
   );
 }
