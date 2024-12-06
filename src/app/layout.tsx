6e02f47156c5ad7c008f0af932824480ed49cd9d
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-06 09:40:12 -0300

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,12 @@ import "./globals.css";
 const alegreya = Alegreya({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Jessica Bandeira",
+  title: "Jessica Bandeira - Linktree",
   description:
     "Sou a Jessica, e este é meu Linktree! Aqui você encontra todos os links importantes e maneiras de entrar em contato comigo de forma rápida e fácil.",
+    icons: {
+      icon: '/avatar.jpg'
+    }
 };
 
 export default function RootLayout({
