868d8c61204b10c49efa978d40e97e9724762a2c
Jessica Bandeira <jessica.santosb@outlook.com>
2024-12-06 08:50:49 -0300

    feat: create icons component

---
+++ src/components/icons.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+
+export function Icons() {
+  return (
+    <div className="flex items-center justify-center gap-4 select-none">
+      <Image
+        alt="javascript"
+        src={
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"
+        }
+        width={40}
+        height={40}
+      />
+      <Image
+        alt="typescript"
+        src={
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg"
+        }
+        width={40}
+        height={40}
+      />
+      <Image
+        alt="react"
+        src={
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"
+        }
+        width={40}
+        height={40}
+      />
+      <Image
+        alt="next"
+        src={
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg"
+        }
+        width={40}
+        height={40}
+      />
+    </div>
+  );
+}
